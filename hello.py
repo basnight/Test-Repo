9170b30c3aceca00ed0891d63246c457625e3000
doug <doug@ON9.local>
2025-08-03 18:58:40 -0400

    updated simple app code

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+print("Welcome to the App Name Generator!")
+color = (input("What is your favorite color? "))
+element = (input("What is your favorite element: Earth, Wind, Fire, Water? "))
+print ("Your new App name could be: " + color +" "+ element)
+
